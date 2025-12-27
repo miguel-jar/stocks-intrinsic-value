3ecf522cc919b6590428532c27ab458cd60cefa4
miguel-jar <miguelabdala90@gmail.com>
2025-12-26 22:52:27 -0300

    implementation of slider + all stocks tab

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import streamlit as st
-import plotly.express as ex
+import pandas as pd
 
-from index_current_comp_iv import index_current_comp_iv
+from calculate_iv import calculate_index_stocks_iv, calculate_b3_all_stocks_iv
 from plot_charts import (
-    plot_upside_chat,
-    plot_price_iv_chat,
-    plot_upside_hist,
-    plot_pe_upside_chat,
     plot_box_plot,
+    plot_pe_upside_chat,
+    plot_price_iv_chat,
+    plot_upside_chat,
+    plot_upside_hist,
 )
-
 
 if __name__ == "__main__":
     indexes = ["IBOV", "SMLL", "IDIV"]
 
     st.set_page_config(page_title="Intrinsic Value Analysis", layout="wide")
-    st.title("📈 Graham Intrinsic Value Analysis")
-    tabs = st.tabs(indexes)
+    st.title("📈 Graham Intrinsic Value Analysis")  
+    tabs = st.tabs(indexes + ["All stocks"])
 
-    for tab, index in zip(tabs, indexes):
-        valid_stocks, invalid_stocks = index_current_comp_iv(index)
+    data = [calculate_index_stocks_iv(index) for index in indexes]
+    data.append(calculate_b3_all_stocks_iv())
+
+    for n in range(len(tabs)):
+        valid_stocks, invalid_stocks = data[n]
+        tab = tabs[n]
 
         with tab:
-            st.subheader(f"Valid stocks - {index}")
+            value = st.slider("Market Cap Filter", min_value=valid_stocks["market_cap"].min(), max_value=valid_stocks["market_cap"].max(), step=1000.0)
+            print(value)
+            valid_stocks = valid_stocks.query("market_cap >= @value")
+            invalid_stocks = pd.concat([invalid_stocks, valid_stocks.query("market_cap < @value")])
+
+            st.subheader("Valid stocks")
             with st.expander("Hide / Show", expanded=True):
                 st.dataframe(
                     valid_stocks,
@@ -57,8 +65,8 @@ if __name__ == "__main__":
                     },
                 )
 
-            st.plotly_chart(plot_upside_chat(valid_stocks.copy(), index))
-            st.plotly_chart(plot_price_iv_chat(valid_stocks.copy(), index))
-            st.plotly_chart(plot_upside_hist(valid_stocks.copy(), index))
-            st.plotly_chart(plot_pe_upside_chat(valid_stocks.copy(), index))
-            st.plotly_chart(plot_box_plot(valid_stocks.copy(), index))
+            st.plotly_chart(plot_upside_chat(valid_stocks.copy()))
+            st.plotly_chart(plot_price_iv_chat(valid_stocks.copy()))
+            st.plotly_chart(plot_upside_hist(valid_stocks.copy()))
+            st.plotly_chart(plot_pe_upside_chat(valid_stocks.copy()))
+            st.plotly_chart(plot_box_plot(valid_stocks.copy()))
