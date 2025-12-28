d9cefe64e8d0a804fe84179a7f3ac07290155296
miguel-jar <miguelabdala90@gmail.com>
2025-12-28 00:31:04 -0300

    list of str as param for func

--- calculate_iv.py
+++ calculate_iv.py
@@ -1,4 +1,5 @@
 from typing import Literal
+
 import pandas as pd
 
 from tools.get_b3_stocks_stats import get_b3_stocks_stats
@@ -21,29 +22,38 @@ def _calculate_iv(stocks_stats: pd.DataFrame) -> list[pd.DataFrame]:
     ]
 
     mask = "(bvps > 0 & eps > 0 & price > 0)"
-    invalid_stocks = df.query(f"not {mask}").reset_index(drop=True)
     valid_stocks = df.query(mask).reset_index(drop=True)
+    invalid_stocks = df.query(f"not {mask}").copy().reset_index(drop=True)
+    # Copy in invalid_stocks to avoid new columns
 
     valid_stocks = valid_stocks.assign(
-        iv=lambda x: ((x["bvps"] * x["eps"] * 22.5) ** (1 / 2)).round(2),
-        p_iv=lambda x: (x["price"] / x["iv"]).round(2),
-        ms=lambda x: (1 - x["p_iv"]).round(2),
-        upside=lambda x: (1 / x["p_iv"] - 1).round(2),
+        intrinsic_value=lambda x: ((x["bvps"] * x["eps"] * 22.5) ** (1 / 2)).round(2),
+        p_iv=lambda x: (x["price"] / x["intrinsic_value"]).round(2),
+        margin_of_security=lambda x: (1 - x["p_iv"]) * 100,
+        upside=lambda x: ((1 / x["p_iv"] - 1)* 100).round(2),
     )
-    
+
     return [valid_stocks, invalid_stocks]
 
 
 def calculate_index_stocks_iv(
-    index: Literal["IBOV", "SMLL", "IDIV"],
-) -> list[pd.DataFrame]:
-    index_stocks = get_index_current_comp(index)
+    indexes: Literal["IBOV", "SMLL", "IDIV"] | list[Literal["IBOV", "SMLL", "IDIV"]],
+) -> dict[str, list[pd.DataFrame]]:
+    if isinstance(indexes, str):
+        indexes = [indexes]
+
     stocks_stats = get_b3_stocks_stats()
-    index_stocks_stats = stocks_stats[
-        stocks_stats["ticker"].isin(index_stocks["ticker"])
+
+    indexes_stocks = [get_index_current_comp(index) for index in indexes]
+    indexes_stocks_stats = [
+        stocks_stats[stocks_stats["ticker"].isin(index_stocks["ticker"])]
+        for index_stocks in indexes_stocks
     ]
 
-    return _calculate_iv(index_stocks_stats)
+    return {
+        index: _calculate_iv(stocks)
+        for index, stocks in zip(indexes, indexes_stocks_stats)
+    }
 
 
 def calculate_b3_all_stocks_iv() -> list[pd.DataFrame]:
@@ -51,6 +61,8 @@ def calculate_b3_all_stocks_iv() -> list[pd.DataFrame]:
 
 
 if __name__ == "__main__":
-    df1, df2 = calculate_index_stocks_iv("IBOV")
-    print(df1)
-    print(df2)
+    results = calculate_index_stocks_iv(["IBOV", "IDIV"])
+    for index, data in results.items():
+        print("\n" + index + "\n")
+        print(data[0])
+        print(data[1])
